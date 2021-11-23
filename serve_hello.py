c53212eb41cf1912c05955ec42243cb594f7df77
javi-redondo <53356357+javi-redondo@users.noreply.github.com>
2021-11-22 18:48:11 -0800

    Create serve_hello.py

---
+++ serve_hello.py
@@ -0,0 +1,13 @@
+import ray
+from ray import serve
+
+ray.init()
+
+serve.start(detached=True)
+
+@serve.deployment
+def hello(request):
+   name = request.query_params["name"]
+   return f"Hello {name}!"
+
+hello.deploy()
