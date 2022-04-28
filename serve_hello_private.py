e35332eb7e7074bc1280cb478da98cfbf9fe7315
Nikita Vemuri <nikitavemuri@gmail.com>
2022-04-28 13:54:14 -0700

    add private serve deployment example

---
+++ serve_hello_private.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from ray import serve
+
+# Let serve HTTP server listen on all network interfaces
+# so deployment is available throughout the Anyscale cloud.
+serve.start(detached=True, http_options={"host": "0.0.0.0"})
+
+app = FastAPI()
+
+@serve.deployment(route_prefix="/")
+@serve.ingress(app)
+class HelloWorld:
+    @app.get("/")
+    def hello(self):
+        return f"Hello world!"
+    
+    @app.get("/healthcheck")
+    def healthcheck(self):
+        return
+
+HelloWorld.deploy()
