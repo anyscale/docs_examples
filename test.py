c60eec34f95568238024b058bda79a8d6458adc5
Nikita Vemuri <nikitavemuri@gmail.com>
2022-05-02 10:47:15 -0700

    wip

---
+++ test.py
@@ -0,0 +1,33 @@
+import numpy as np
+import wandb
+
+from ray import tune
+from ray.tune import Trainable
+from ray.tune.integration.wandb import (
+    WandbLoggerCallback,
+    WandbTrainableMixin,
+    wandb_mixin,
+)
+
+def objective(config, checkpoint_dir=None):
+    for i in range(30):
+        loss = config["mean"]
+        tune.report(loss=loss)
+
+def tune_function():
+    """Example for using a WandbLoggerCallback with the function API"""
+    analysis = tune.run(
+        objective,
+        metric="loss",
+        mode="min",
+        config={
+            "mean": tune.grid_search([1, 2, 3, 4, 5]),
+        },
+        callbacks=[
+            WandbLoggerCallback(project="Wandb_example")
+        ],
+    )
+    return analysis.best_config
+
+
+tune_function()
