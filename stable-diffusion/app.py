138e99c36efca2cab2252a97f02fd84584fbec47
simon-mo <simon.mo@hey.com>
2022-11-30 12:50:31 -0800

    update stable diffusion v2 with fastapi

--- stable-diffusion/app.py
+++ stable-diffusion/app.py
@@ -1,30 +1,42 @@
 from io import BytesIO
 
-import numpy as np
-from PIL import Image
 from ray import serve
 from fastapi import FastAPI
 from fastapi.responses import Response
+import torch
+from diffusers import EulerDiscreteScheduler, StableDiffusionPipeline
 
 app = FastAPI()
+
+@serve.deployment(num_replicas=1, route_prefix="/")
+@serve.ingress(app)
+class APIIngress:
+    def __init__(self, diffusion_model_handle) -> None:
+        self.handle = diffusion_model_handle
+
+    @app.get(
+        "/imagine",
+        responses={200: {"content": {"image/png": {}}}},
+        response_class=Response,
+    )
+    async def generate(self, prompt: str, img_size: int = 512):
+        assert len(prompt), "prompt parameter cannot be empty"
+
+        image = await (await self.handle.generate.remote(prompt, img_size=img_size))
+
+        file_stream = BytesIO()
+        image.save(file_stream, "PNG")
+        return Response(content=file_stream.getvalue(), media_type="image/png")
 
 
 @serve.deployment(
     ray_actor_options={"num_gpus": 1},
-    # num_replicas=1,
     autoscaling_config={"min_replicas": 0, "max_replicas": 2},
-    route_prefix="/",
 )
-@serve.ingress(app)
-class StableDifussionV2:
+class StableDiffusionV2:
     def __init__(self):
-        import numpy as np
-        import torch
-        from diffusers import EulerDiscreteScheduler, StableDiffusionPipeline
-
         model_id = "stabilityai/stable-diffusion-2"
 
-        # Use the Euler scheduler here instead
         scheduler = EulerDiscreteScheduler.from_pretrained(
             model_id, subfolder="scheduler"
         )
@@ -33,19 +45,14 @@ class StableDifussionV2:
         )
         self.pipe = self.pipe.to("cuda")
 
-    @app.get(
-        "/imagine",
-        responses={200: {"content": {"image/png": {}}}},
-        response_class=Response,
-    )
     def generate(self, prompt: str, img_size: int = 512):
         assert len(prompt), "prompt parameter cannot be empty"
 
         image = self.pipe(prompt, height=img_size, width=img_size).images[0]
 
-        file_stream = BytesIO()
-        image.save(file_stream, "PNG")
-        return Response(content=file_stream.getvalue(), media_type="image/png")
+        return image
 
 
-v2_model = StableDifussionV2.bind()
+
+entrypoint = APIIngress.bind(StableDiffusionV2.bind())
+
