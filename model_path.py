98b195e35765f59dc827c8b411950d8f31a6c499
Edward Oakes <ed.nmi.oakes@gmail.com>
2023-01-31 12:48:14 -0800

    fix

---
+++ model_path.py
@@ -0,0 +1,20 @@
+from typing import Any, Dict
+
+from ray import serve
+
+from fastapi import FastAPI
+
+f = FastAPI()
+
+@serve.deployment(user_config={"model_path": "/mnt/shared_storage/model_v0"})
+@serve.ingress(f)
+class ModelLoader:
+    def reconfigure(self, d: Dict[str, Any]):
+        self._path = d["model_path"]
+        print(f"Loading model from path: {self._path}")
+
+    @f.get("/predict")
+    def predict(self) -> str:
+        return f"Response from model at {self._path}"
+
+d = ModelLoader.bind()
